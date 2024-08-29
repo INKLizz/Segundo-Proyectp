66be342a5c88dc02a590eb9cf2e0af9635bb5039
Fhernandez20 <fdhg0942@gmail.com>
2024-08-28 21:27:37 -0600

    Add files via upload
    Signed-off-by: Fhernandez20 <fdhg0942@gmail.com>

---
+++ Segundo-Proyectp-main/src/segundo_proyect/Tweets.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package segundo_proyect;
+import java.util.Calendar;
+/**
+ *
+ * @author Laura Sabillon
+ */
+public class Tweets {
+    
+    // VARIABLES
+    private String twit;
+    private USUARIO user;
+    private Calendar publicado;
+
+    // CONSTRUCTOR
+    public Tweets(String twit, USUARIO user) {
+        this.twit = twit;
+        this.user = user;
+        this.publicado = Calendar.getInstance();
+    }
+
+    // GETTERS
+    public String getContent() {
+        return twit;
+    }
+
+    public USUARIO getUser() {
+        return user;
+    }
+
+    public Calendar getFechaPublicada() {
+        return publicado;
+    }
+
+    // SETTERS
+    public void setContent(String twit) {
+        this.twit = twit;
+    }
+
+    public void setUser(USUARIO user) {
+        this.user = user;
+    }
+
+    public void setFechaPublicada(Calendar publicado) {
+        this.publicado = publicado;
+    }
+
+    //PUBLICAR
+    public void publicarTweet() {
+            System.out.println("Autor: " + user.getUsuario());
+            System.out.println(twit);
+            System.out.println("Fecha de publicacion: " + publicado.getTime());
+    }
+}
---
+++ Segundo-Proyectp-main/src/segundo_proyect/users.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package segundo_proyect;
+
+/**
+ *
+ * @author Laura Sabillon
+ */
+public class users {
+    
+    //LLAMMAR VARIABLES
+    private USUARIO usuarios [];    
+    private static USUARIO EnSesion = null;    
+    
+    public users (int limite){
+        usuarios = new USUARIO[limite];
+    }
+    
+    //BUSCAR Y AGREGAR EL USUARIO (SI YA NO ESTA EN EL SISTEMA)
+    USUARIO buscar(String usuario) {
+        for (USUARIO use : usuarios) {
+            if (use != null && use.getUsuario().equals(usuario)) {
+                return use;
+            }
+        }
+        return null;
+    }
+    
+    public boolean agregarUsers(String nombre, String usuario, char genero, int edad, String password){
+        if (buscar(usuario) == null){
+            for (int indice = 0; indice < usuarios.length ; indice++){
+                if (usuarios[indice] == null){
+
+                    usuarios [indice] = new USUARIO (nombre, usuario, genero, edad, password);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+        
+    //VERIFICAR LOGIN INICIADO
+    public boolean Login(String usuario, String password) {
+        USUARIO user = buscar(usuario);
+        if (user != null && user.getPassword().equals(password)) {
+            logged_in.setEnSesion(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
